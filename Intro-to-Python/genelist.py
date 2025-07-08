2f357e5f5253bb38af4d54378e166f4cbc76f876
Anderson Paulo Ávila Santos <anderson.santos@nees.ufal.br>
2025-07-08 19:47:43 -0300

    update notebooks

--- Intro-to-Python/genelist.py
+++ Intro-to-Python/genelist.py
@@ -1,11 +1,11 @@
 import sys
 
-def get_genes(infile,outfile):
+def get_genes(infile, outfile):
     """
     Function to extract a list of genes and write to file
     """
     gene_list = []
-    with open(infile) as  gene:
+    with open(infile) as gene:
         tag = False
         for line in gene:
             if line.startswith('name'):
@@ -16,11 +16,16 @@ def get_genes(infile,outfile):
                 if len(items) > 0:
                     gene_list.append(items[0])
     gene_list = gene_list[1:-7]
-    with open(outfile, 'w') as outfile:
+    with open(outfile, 'w') as out:
         for i in gene_list:
-            outfile.write(i+'\n')
+            out.write(i + '\n')
     return True
 
-infile = sys.argv[1]
-output = sys.argv[2]
-get_genes(infile,output)
+# Protege para só executar via linha de comando
+if __name__ == "__main__":
+    if len(sys.argv) < 3:
+        print("Usage: python genelist.py <input_file> <output_file>")
+    else:
+        infile = sys.argv[1]
+        output = sys.argv[2]
+        get_genes(infile, output)
